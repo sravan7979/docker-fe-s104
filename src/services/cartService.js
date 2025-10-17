11503f41f70a0686c394fdf0774366b8ff0845f1
Maturu Sravan Kumar <148370583+sravan7979@users.noreply.github.com>
2025-10-17 23:18:06 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-164-180-51.compute-1.amazonaws.com:8081/api/cart";
+const API_URL = "http://ec2-54-221-35-198.compute-1.amazonaws.com:8081/api/cart";
 
-const API_BASE = "http://ec2-54-164-180-51.compute-1.amazonaws.com:8081/api/cart";
+const API_BASE = "http://ec2-54-221-35-198.compute-1.amazonaws.com:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
@@ -29,5 +29,6 @@ export const removeCartItem = async (cartItemId) => {
         console.error("Error removing item from cart", error);
     }
 };
+
 
 
